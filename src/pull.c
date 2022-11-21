8c44a5ab2292aff7d3442b402ed8b9d53009a54d
Igor Parfenov <parfenov_2001@mail.ru>
2022-11-21 20:18:53 +0300

    Version 0.5

--- src/pull.c
+++ src/pull.c
@@ -6,8 +6,10 @@ void umount_disk(char *argv[]);
 void pull(char *argv[]) {
     char cmd[1024];
 
-    if (system("rm -rf tmp/."))
+    if (system("rm -rf tmp"))
         errExit("rm");
+    if (system("mkdir tmp"))
+        errExit("mkdir");
 
     snprintf(cmd, 1024, "docker export $(docker create %s) | tar -C tmp -xvf -", argv[1]);
     if (system(cmd))
@@ -18,8 +20,11 @@ void pull(char *argv[]) {
     snprintf(cmd, 1024, "cp -rf tmp/. mnt/%s", argv[0]);
     if (system(cmd))
         errExit("cp");
-    if (system("rm -rf tmp/."))
+
+    if (system("rm -rf tmp"))
         errExit("rm");
+    if (system("mkdir tmp"))
+        errExit("mkdir");
 
     umount_disk(argv);
 }
